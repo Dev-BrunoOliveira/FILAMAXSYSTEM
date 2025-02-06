c3fdf8e2265abb9d2ba5868cab0b271a52e4b478
Bruno Oliveira <brunooliver2015@outlook.comm>
2025-02-06 12:31:57 -0300

    configurando banco de dados

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
-const escalaRef = ref(database, "Escala");
+import { database, ref, set, onValue } from "./firebase.js";
 
+const escalaRef = ref(database, "Escala");
 let escala = {};
 
 onValue(escalaRef, (snapshot) => {
   const data = snapshot.val();
+  console.log("Dados recebidos do Firebase:", data); // Debug
   if (data) {
     escala = data;
     atualizarCalendario();
@@ -12,7 +14,7 @@ onValue(escalaRef, (snapshot) => {
 
 function salvarEscala(dia, editores) {
   const diaRef = ref(database, `Escala/${ano}-${mes}/${dia}`);
-  set(diaRef, editores)
+  set(diaRef, { editores })
     .then(() => {
       console.log(`Escala do dia ${dia} salva com sucesso.`);
     })
@@ -38,7 +40,9 @@ function atualizarCalendario() {
       select.appendChild(option);
     });
 
-    const editoresDia = escala[`${ano}-${mes}`]?.[dia] || [];
+    const editoresDia = escala[`${ano}-${mes}`]
+      ? escala[`${ano}-${mes}`][String(dia)] || []
+      : [];
     select.value = editoresDia[0] || "";
 
     select.addEventListener("change", () => {
